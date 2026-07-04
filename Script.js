0c2686ad09fdba1ca8d49a952c641140b39fa1d9
Awsaf7 <chowdhuryawsaf7@gmail.com>
2026-07-04 21:43:53 +0600

    3rd commit

---
+++ Script.js
@@ -0,0 +1,11 @@
+function showMessage() {
+  alert("Thank you for visiting Kitty Pure! We sell the best cat litter, dry food, and wet food for your cat.");
+}
+
+function addToCart(productName) {
+  alert(productName + " has been added to your cart!");
+}
+
+function contactInfo() {
+  alert("You can reach us at 01698742692. Visit us at Gulshan 2, Road 108, House 70.");
+}
